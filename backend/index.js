4ddd5ccdc6610a303278e56cdb96065454b7dddc
ajinkyachirde07 <ajinkyachirde07@gmail.com>
2025-08-31 13:59:11 +0530

    ul

--- backend/index.js
+++ backend/index.js
@@ -18,7 +18,8 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
 const corsOptions = {
-    origin:'http://localhost:5173',
+    // origin:'http://localhost:5173',
+    origin:'https://jobportal-website-1eh9.onrender.com',
     credentials:true
 }
 

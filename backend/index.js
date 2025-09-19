42d53661cd7a9eb444c49a76b6ff691da354f74f
ajinkyachirde07 <ajinkyachirde07@gmail.com>
2025-09-19 13:00:30 +0530

    cookie change

--- backend/index.js
+++ backend/index.js
@@ -28,7 +28,7 @@ app.use(cookieParser());
 app.use(
   cors({
     origin: ["https://job-portal-website-khaki.vercel.app","http://localhost:5173"],       // your frontend origin
-    credentials: true, // allow cookies / authorization headers
+    credentials:true, // allow cookies / authorization headers
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
   })

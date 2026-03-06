8a8a7e01a8f2e94e5bb420da760da545416ec090
ajinkyachirde07 <ajinkyachirde07@gmail.com>
2026-03-06 10:22:28 +0530

    jobs changed

--- frontend/src/components/Job.jsx
+++ frontend/src/components/Job.jsx
@@ -1,5 +1,5 @@
 import { Button } from './ui/button'
-import { Bookmark } from 'lucide-react'
+// import { Bookmark } from 'lucide-react'
 import { Avatar, AvatarImage } from './ui/avatar'
 import { Badge } from './ui/badge'
 import { useNavigate } from 'react-router-dom'
